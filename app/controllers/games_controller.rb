281c5d19695098f80976b7077e113579baeb8fb8
Maria Micaela Vidal <131964698+mikividal@users.noreply.github.com>
2025-05-05 22:00:25 +0100

    everything working ok

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,3 +1,4 @@
+require "open-uri"
 class GamesController < ApplicationController
   def new
     #generate an array of 10 random letters and store them as @letters
@@ -10,14 +11,18 @@ class GamesController < ApplicationController
 
   def score
     @word = params[:game]
-    myword = params[:randomletters].split
-    p myword
+    @myword = params[:randomletters]
+    @included = included?(@word, @myword)
+    @english_word = english_word?(@word)
+  end
 
-    if myword.include?(@word.split)
-      @result = "good job"
-    else
-      @result = "not valid"
-      raise
-    end
+  def english_word?(word)
+    response = URI.open("https://dictionary.lewagon.com/#{word}")
+    json = JSON.parse(response.read)
+    json['found']
+  end
+
+  def included?(word, letters)
+    word.chars.all? { |letter| word.count(letter) <= letters.count(letter)}
   end
 end
